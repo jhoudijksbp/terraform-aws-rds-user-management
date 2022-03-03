c81bc8591a6f487791b361627ea1591c278048a3
Jeffrey Houdijk <jhoudijk@schubergphilis.com>
2022-03-03 14:57:03 +0100

    Enable management of Master users

--- rds_users.tf
+++ rds_users.tf
@@ -125,3 +125,57 @@ resource "aws_cloudformation_stack" "execute_lambda_user_management" {
     module.rds_user_management_lambda
   ]
 }
+
+# Random password generator for master users
+resource "random_password" "db_master_pass" {
+  for_each         = { for user in local.sql_users_map : user.unique_name => user if user.master_user }  
+  length           = 40
+  special          = true
+  min_special      = 5
+  override_special = "!#$%^&*()-_=+[]{}<>:?"
+  keepers = {
+    pass_version = 1
+  }
+}
+
+# Create a secret for Master users
+resource "aws_secretsmanager_secret" "db_master_user" {
+  for_each   = { for user in local.sql_users_map : user.unique_name => user if user.master_user }
+  name       = "db_master_user_${each.value.unique_name}"
+  kms_key_id = var.kms_key_id
+  tags       = merge(var.tags, { "SECRET_TYPE" = "MASTER_RDS" })
+}
+
+# Create a secret version for Master users 
+resource "aws_secretsmanager_secret_version" "db_master_user_secret_version" {
+  for_each  = { for user in local.sql_users_map : user.unique_name => user if user.master_user }
+  secret_id = aws_secretsmanager_secret.db_master_user[each.value.unique_name].id
+
+  secret_string = jsonencode({
+    authentication       = each.value.authentication,
+    dbInstanceIdentifier = each.value.rds_cluster_identifier,
+    engine               = "mysql",
+    host                 = each.value.rds_endpoint,
+    password             = random_password.db_master_pass[each.value.unique_name].result,
+    port                 = each.value.rds_port,
+    src_host             = each.value.src_host,
+    username             = each.value.username
+  })
+
+  lifecycle {
+    ignore_changes = [
+      secret_string
+    ]
+  }
+}
+
+# Enable password rotation for master users
+resource "aws_secretsmanager_secret_rotation" "db_master_user_secret_rotation" {
+  for_each            = { for user in local.sql_users_map : user.unique_name => user if user.rotation && user.master_user && var.deploy_password_rotation }
+  secret_id           = aws_secretsmanager_secret.db_user[each.value.unique_name].id
+  rotation_lambda_arn = module.rds_password_rotation[0].arn
+
+  rotation_rules {
+    automatically_after_days = 30
+  }
+}
