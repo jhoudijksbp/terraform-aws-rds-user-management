fb1baf822ec62216ed33ef7b7800bd534e0c46ff
Jeffrey Houdijk <jhoudijk@schubergphilis.com>
2022-03-03 10:55:52 +0100

    fix if

--- vpc_endpoints.tf
+++ vpc_endpoints.tf
@@ -20,7 +20,7 @@ resource "aws_security_group_rule" "vpc_endpoint_secrets_manager_sg_rule" {
 }
 
 resource "aws_security_group_rule" "vpc_endpoint_secrets_manager_sg_pwd_rule" {
-  count                    = "${var.create_vpc_secm_endpoint == true && deploy_password_rotation == true ? 1 : 0}"
+  count                    = "${var.create_vpc_secm_endpoint == true && var.deploy_password_rotation == true ? 1 : 0}"
 
   description              = "Allow Ingress traffic over port 443 to this endpoint"
   from_port                = 443
